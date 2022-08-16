cfa254672a78bfd940b6a49ab231fa77dd8eb098
cerysm <nappidybap@gmail.com>
2022-08-16 19:05:03 +0100

    (finally) sorted the ftp stuff
    was overcomplicating, have stopped doing that now

--- src/ftpserver.py
+++ src/ftpserver.py
@@ -1,9 +1,62 @@
-#filezilla admin pword: bepis3000 , user login = user, password = thisisapassword
 from ftplib import FTP
+import os
 
-#serverip:
-ftp = FTP('127.0.0.1')
-ftp.login(user='user', passwd = 'thisisapassword')
-ftp.mkd('newDir')
-ftp.cwd('newDir')
-ftp.retrlines('LIST')
+# adds the zip file to the ftpserver
+def setup():
+
+    # setup the ftp connection (yeah its in local storage port 21 is blocked on ethernet)
+    ftp = FTP()
+    # should be runnning through serverStart.py, change those credentials if needed
+    ftp.connect(host='127.0.0.1', port=2121)
+    ftp.login(user='user', passwd='password')
+    # creates the folder to store the samples
+    try:
+        ftp.cwd('/samples')
+    except:
+        ftp.mkd('/samples')
+        ftp.cwd('/samples')
+
+    os.chdir('test/samples')
+    # gets every file in the sample folder
+    lst = os.walk(os.getcwd())
+    for file in lst:
+        print(file[0])
+        path = file[0]
+        try:
+            for csv in file[2]:
+                f = open(f'{path}\\{csv}', 'rb')
+                ftp.storbinary(f'STOR {csv}', f)
+                f.close()
+        except:
+            continue
+        ftp.cwd('\\samples\\')
+    os.chdir('..')
+# adds the zip file to the ftpserver
+def pullSamples(date):
+    # another connection! [because when this is run the initial one may have timed out, same stuff though]
+    ftp = FTP()
+    # should be runnning through serverStart.py, change those credentials if needed
+    ftp.connect(host='127.0.0.1', port=2121)
+    ftp.login(user='user', passwd='password')
+    # should error handle this but there are no circumstances where this is called pre setup
+    ftp.cwd('\\samples')
+    os.chdir('..')
+
+    try:
+        os.chdir('tmp')
+    except:
+        os.mkdir('tmp')
+        os.chdir('tmp')
+
+    files = ftp.nlst()
+    returnLst = []
+    for x in files:
+        if date in x:
+            with open(x, 'wb') as fp:
+                ftp.retrbinary(f'RETR {x}', fp.write)
+    #ftp.cwd('\\samples')
+    #ftp.quit()
+
+# for testing!
+#setup()
+#pullSamples(20220803)
---
+++ src/serverStart.py
@@ -0,0 +1,31 @@
+#THIS HAS NO SHUTDOWN COMMAND RIGHT NOW! WILL ADD ONE AS SOON AS I FIGURE OUT HOW TO
+
+# new file to stop relying on filezila
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib import servers
+from pyftpdlib.handlers import FTPHandler
+import time
+
+def startServer():
+    authorizer = DummyAuthorizer()
+
+    authorizer.add_user('user', 'password', '.', perm='elradfmwMT')
+    # authorizer.add_anonymous(os.getcwd())
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "ready!"
+
+    # changed the server around, this now starts a simple ftpserver on 0.0.0.0:12345
+    address = ("127.0.0.1", 2121)
+    server = servers.FTPServer(address, handler)
+    server.serve_forever()
+
+
+startServer()
+time.sleep(5)
+#self.close_all()
